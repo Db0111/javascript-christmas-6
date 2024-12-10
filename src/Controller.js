61627633faef5a7e9649ffee14a8a19f2f38f9df
deokbin <deokbin@naver.com>
2024-12-10 12:14:46 +0900

    feat(Controller): 날짜 및 메뉴 입력 받는 로직 추가

--- src/Controller.js
+++ src/Controller.js
@@ -1,12 +1,25 @@
 import InputValidator from './InputValidator.js';
 import { InputView } from './InputView.js';
+import Menu from './Menu.js';
 import IOMessage from './constants/IOMessage.js';
 
 class Controller {
+  static async processPlan() {
+    await this.getDate();
+    const menu = await this.getMenu();
+    const result = await Menu.splitInput(menu);
+    Menu.validateSplittedInput(result);
+  }
+
   static async getDate() {
     const date = await InputView.getInput(IOMessage.GET_DATE);
     InputValidator.processDateInput(date);
-    return date;
+    return Number(date);
+  }
+
+  static async getMenu() {
+    const menu = await InputView.getInput(IOMessage.GET_MENU);
+    return menu;
   }
 }
 export default Controller;
