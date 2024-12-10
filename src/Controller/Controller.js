73319fce338b83def8bc4c62b47f18bb69e4f01b
deokbin <deokbin@naver.com>
2024-12-10 23:27:15 +0900

    feat(Controller): 혜책 및 보너스, 이벤트 배지 로직 추가

--- src/Controller/Controller.js
+++ src/Controller/Controller.js
@@ -6,7 +6,10 @@ import IOMessage from '../constants/IOMessage.js';
 import formatOrderedList from '../utils/formatOrderedList.js';
 import splitInput from '../utils/splitInput.js';
 import Event from '../Model/Event.js';
-import { getPriceList } from '../getPriceList.js';
+import { getPriceList, getTotalPrice } from '../getPriceList.js';
+import Bonus from '../Model/Bonus.js';
+import Discount from '../Model/Discount.js';
+import Badge from '../Model/Badge.js';
 
 class Controller {
   static async processPlan() {
@@ -14,12 +17,16 @@ class Controller {
     const menu = await this.getMenu();
     const orderedList = Menu.validateSplittedInput(splitInput(menu));
     await this.printMenuList(date, orderedList);
-    console.log(getPriceList(orderedList));
-    const price = 0;
-    await this.printTotalPrice(price);
+    const priceList = getPriceList(orderedList);
+    const totalPrice = getTotalPrice(priceList);
+    await this.printTotalPrice(totalPrice);
+    const bonusPrice = Bonus.calculateBonusPrice(totalPrice);
     const eventData = Event.processEvent(date);
-    // Discount.calculateTotalPrice(orderedList);
-    // Discount.calculateDiscount(date, eventData);
+    const discountTotalData = Discount.calculateDiscount(date, eventData, priceList, bonusPrice);
+    await this.printDiscountList(Discount.discountData);
+    await this.printTotalDiscount(discountTotalData, bonusPrice);
+    await this.printFinalPrice(totalPrice, discountTotalData);
+    await this.printBadge(discountTotalData, bonusPrice);
   }
 
   static async getDate() {
@@ -42,6 +49,32 @@ class Controller {
   static async printTotalPrice(price) {
     OutputView.printMessage(IOMessage.TOTAL_BEFORE_DISCOUNT);
     OutputView.printMessage(`${price}원`);
+  }
+
+  static async printDiscountList(discountData) {
+    OutputView.printMessage(IOMessage.DISCOUNT_LIST);
+    for (const [discountName, price] of Object.entries(discountData)) {
+      if (price > 0) {
+        OutputView.printMessage(`${discountName}: -${price}원`);
+      }
+    }
+  }
+
+  static async printTotalDiscount(discountTotalData, bonusEventPrice) {
+    OutputView.printMessage(IOMessage.DISCOUNT_TOTAL);
+    OutputView.printMessage(`-${discountTotalData + bonusEventPrice}원`);
+  }
+
+  static async printFinalPrice(totalPrice, discountTotalData) {
+    const finalPrice = totalPrice - discountTotalData;
+    OutputView.printMessage(IOMessage.TOTAL_AFTER_DISCOUNT);
+    OutputView.printMessage(`${finalPrice}원`);
+  }
+
+  static async printBadge(discountTotalData, bonusPrice) {
+    OutputView.printMessage(IOMessage.EVENT_BADGE);
+    const totalPrice = discountTotalData + bonusPrice;
+    OutputView.printMessage(Badge.calculateBadge(totalPrice));
   }
 }
 export default Controller;
