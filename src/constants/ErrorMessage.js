f31a103820c038ed56d7b06f545f061c2fb65f5d
deokbin <deokbin@naver.com>
2024-12-10 12:14:02 +0900

    fix(ErrorMessage): 에러 메시지 문구 수정

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -1,7 +1,7 @@
 const ErrorMessage = Object.freeze({
   INVALID_DATE: `[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.`,
   INVALID_VALUE: `[ERROR] 유효하지 않은 입력입니다. 다시 입력해 주세요.`,
-  NOT_A_NUMBER: `[ERROR] 숫자는 입력할 수 없습니다. 다시 입력해주세요`,
+  NOT_A_NUMBER: `[ERROR] 문자는 입력할 수 없습니다. 다시 입력해주세요`,
   NO_MENU: `[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
   INVALID_ORDERNUM: `[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
   INVALID_ORDER: `[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
