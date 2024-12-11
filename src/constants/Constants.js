6da3cbf69f8ae33b1ed362cabffdec0f69765f19
deokbin <deokbin@naver.com>
2024-12-11 17:21:17 +0900

    feat(Constants): 계행 분리 변수 추가

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,3 +1,5 @@
-export const regex = /^[0-9]*$/;
+export const regex = /^[0-9]+$/;
 
 export const formatRegex = /^\s*^-\s+\s*-\s*\d+\s*\(,^\s*^-\s+\s*-\s*\d+\s*\)*$/;
+
+export const LINE_SEPARATOR = '\n';
