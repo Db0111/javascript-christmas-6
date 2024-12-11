fff400485ce8ea813ad11fefaf1c1b9315fe2881
deokbin <deokbin@naver.com>
2024-12-11 12:16:50 +0900

    refactor(splitInput): 유효성 검사 추가

--- src/utils/splitInput.js
+++ src/utils/splitInput.js
@@ -1,9 +1,18 @@
+import ErrorMessage from '../constants/ErrorMessage.js';
+
 function splitInput(input) {
   const parsedInput = input.split(',');
-  return parsedInput.reduce((acc, item) => {
+  const productMap = {};
+
+  parsedInput.forEach(item => {
     const [name, quantity] = item.split('-');
-    acc[name.trim()] = parseInt(quantity.trim(), 10);
-    return acc;
-  }, {});
+    const trimmedName = name.trim();
+
+    if (productMap[trimmedName]) {
+      throw new Error(`${ErrorMessage.DUPLICATED_ORDER}`);
+    }
+    productMap[trimmedName] = parseInt(quantity.trim(), 10);
+  });
+  return productMap;
 }
 export default splitInput;
